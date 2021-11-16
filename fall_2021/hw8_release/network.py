d51ac004300c57d91a55d1c58548edf2dd00b4ae
leobxpan <pankobe24@gmail.com>
2021-11-15 23:24:55 -0800

    released HW8.

---
+++ fall_2021/hw8_release/network.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        ### YOUR CODE HERE
+        pass
+        ### END YOUR CODE
+
+    def forward(self, x):
+        ### YOUR CODE HERE
+        pass
+        ### END YOUR CODE
+        return x
